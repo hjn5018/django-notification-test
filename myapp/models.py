ee238d9339dd7774873b9abc855d03b803ad75bc
hjn5018 <hjn5018@gmail.com>
2024-09-20 13:40:18 +0900

    Add Notification model

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# 1. 👇 Add the following line
+class Notification(models.Model):
+    message = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.message
